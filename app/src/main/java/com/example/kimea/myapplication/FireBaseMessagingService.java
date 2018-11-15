43f6a936fb74b22d25f51ef2f39040bf1cd6203e
parkge5469 <01021065459a@gmail.com>
2018-11-15 15:46:42 +0900

    group chat room test 1

--- app/src/main/java/com/example/kimea/myapplication/FireBaseMessagingService.java
+++ app/src/main/java/com/example/kimea/myapplication/FireBaseMessagingService.java
@@ -9,6 +9,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.media.RingtoneManager;
@@ -28,7 +29,7 @@ public class FireBaseMessagingService extends FirebaseMessagingService{
     private static final String TAG = "MyFirebaseMsgService";
     private int badge;
     int badge_count;
-    String result;
+    String result, chatRoom;
     SQLiteDatabase db;
     DBHelper helper =  new DBHelper(this);
     @Override
@@ -54,7 +55,8 @@ public class FireBaseMessagingService extends FirebaseMessagingService{
         String msgBody = remoteMessage.getNotification().getBody();
         String msgTitle = remoteMessage.getNotification().getTitle();
         String email = remoteMessage.getData().get("email");
-        String chatRoom = remoteMessage.getData().get("roomname");
+        chatRoom = remoteMessage.getData().get("roomname");
+        String groupYN = remoteMessage.getData().get("group");
         ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         List<ActivityManager.RunningTaskInfo> ac = am.getRunningTasks(1);
         SharedPreferences getEmail = getSharedPreferences("chatEmail",MODE_PRIVATE);
@@ -63,10 +65,25 @@ public class FireBaseMessagingService extends FirebaseMessagingService{
         String ss = array[1];
         String[] ary2 = ss.split("\\.");
         result = array[0] + ary2[0] + ary2[1];
-        insert(email,msgTitle,msgBody,"0");
+
+        try{
+            db = helper.getReadableDatabase();
+            String query = "select * from '"+result+"'";
+            Cursor cur = db.rawQuery(query,null);
+            cur.moveToFirst();
+            insert(email,msgTitle,msgBody,"0");
+
+        }catch (Exception e){
+            db = helper.getWritableDatabase();
+            db.execSQL("create table '"+result+"'(Chatseq integer primary key autoincrement, ChatId text,ChatNickName text, ChatText text, type TEXT);");
+            insert2(chatRoom);
+
+            ((ViewPagerActivity)ViewPagerActivity.CONTEXT).reset();
+        }
+
 
         if(email!=null){
-            SharedPreferences pref = getSharedPreferences(email,MODE_PRIVATE);
+            SharedPreferences pref = getSharedPreferences(chatRoom,MODE_PRIVATE);
             SharedPreferences.Editor editor = pref.edit();
             String badge_check = pref.getString("badge_count","");
             if(badge_check.isEmpty()){
@@ -81,6 +98,7 @@ public class FireBaseMessagingService extends FirebaseMessagingService{
                 SharedPreferences.Editor editor3 = preferences.edit();
                 editor3.putString("badge_count",String.valueOf(badge_int));
                 editor3.commit();
+
                 ((ViewPagerActivity)ViewPagerActivity.CONTEXT).reset();
                 Log.e(TAG,"email_badge_commit");
             }
@@ -98,6 +116,8 @@ public class FireBaseMessagingService extends FirebaseMessagingService{
             set_badge_alarm(badge_count);
         }
         Log.i("Fire Service", email+"/"+msgTitle+"/"+msgBody+"/"+result);
+
+
 
     }
     private void sendNotification(String messageBody,String messageTitle) {
@@ -130,10 +150,37 @@ public class FireBaseMessagingService extends FirebaseMessagingService{
         values.put("ChatId", id);
         values.put("type",type);
         values.put("ChatText",text);
-
         db.insert("'"+result+"'", null, values); // 테이블/널컬럼핵/데이터(널컬럼핵=디폴트)
+        /*try{
+
+        }catch (Exception e){
+            db = helper.getWritableDatabase();
+            db.execSQL("create table '"+result+"'(Chatseq integer primary key autoincrement, ChatId text,ChatNickName text, ChatText text, type TEXT);");
+            db.insert("'"+result+"'", null, values);
+            ChattingTabFragment ctf = new ChattingTabFragment();
+            ctf.addProfile(id,text,null,"",chatRoom);
+            Log.i("create","createGroup");
+        }*/
         Log.i("insert","insert");
         // tip : 마우스를 db.insert에 올려보면 매개변수가 어떤 것이 와야 하는지 알 수 있다.
+    }
+    public void insert2(String id) {
+        db = helper.getWritableDatabase(); // db 객체를 얻어온다. 쓰기 가능
+        ContentValues values2 = new ContentValues();
+        // db.insert의 매개변수인 values가 ContentValues 변수이므로 그에 맞춤
+
+        // 데이터의 삽입은 put을 이용한다.
+        values2.put("userId", id);
+        db.insert("oneUser", null, values2); // 테이블/널컬럼핵/데이터(널컬럼핵=디폴트)
+        Log.i("insert","insert");
+        // tip : 마우스를 db.insert에 올려보면 매개변수가 어떤 것이 와야 하는지 알 수 있다.
+        SQLiteDatabase database = helper.getReadableDatabase();
+        String sql = "select * from oneUser;";
+        Cursor cursor2 = database.rawQuery(sql, null);
+        while(cursor2.moveToNext()){
+            Log.i("id1",cursor2.getString(0));
+            Log.i("id1",cursor2.getString(1));
+        }
     }
     public void set_badge_alarm(int badge_count){
         Log.e(TAG , "badgeCount :"+badge_count);
